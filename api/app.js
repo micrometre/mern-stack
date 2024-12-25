b71cc9bd8c86a90f6ec98939702034073abaa34e
Henok Wehibe <henokwehibe@gmail.com>
2024-12-25 20:32:28 +0000

    sorted mern

--- api/app.js
+++ api/app.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+var db = require('./config/db')
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
